0059d02fe3ccfab3e4cfbca9a4bd21ac1ecb402b
LewisIvy <592131702@qq.com>
2015-01-14 17:25:38 +0800

    Update digest_binlog_dml.sh

--- digest_binlog_dml.sh
+++ digest_binlog_dml.sh
@@ -13,6 +13,7 @@ if [ $last_binlog_name = $binlog_name ]; then
         echo "没有要分析的binlog！"
 else
         echo "+-------+------------+---------`date +%Y%m%d%H%M%S` $binlog_name ------------+------+-------------+---------+" >> $last_binlog_name_path/$dt.Digest_Binlog.log;
-        mysqlbinlog --no-defaults -v -v /data/mysql/$binlog_name    |grep -E "UPDATE|INSERT|DELETE" |awk '{print $NF}' |sort  |uniq -c |sort -rn |head -100  >> $last_binlog_name_path/$dt.Digest_Binlog.log;
+        #mysqlbinlog --no-defaults -v -v /data/mysql/$binlog_name    |grep -E "UPDATE|INSERT|DELETE" |awk '{print $NF}' |sort  |uniq -c |sort -rn |head -100  
+        mysqlbinlog --no-defaults -v -v /data/mysql/$binlog_name   |grep -E "UPDATE|INSERT|DELETE" |sort -t ' ' |uniq -c |sort -rn |head -100 >> $last_binlog_name_path/$dt.Digest_Binlog.log;
         echo $binlog_name >> $last_binlog_name_path/last_binlog_name.log;
 fi
